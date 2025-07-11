100cdc2b8b75e6a7e195e97c4ae81e7b37d62209
mahirbugray <74426348+mahirbugray@users.noreply.github.com>
2025-07-12 01:21:49 +0300

    FirstCommit

---
+++ MultiShop/Serices/Order/Infrastructures/MultiShop.Order.Persistance/Migrations/20250709150133_mig3.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultiShop.Order.Persistance.Migrations
+{
+    public partial class mig3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductId",
+                table: "OrderDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "OrderDetails",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
