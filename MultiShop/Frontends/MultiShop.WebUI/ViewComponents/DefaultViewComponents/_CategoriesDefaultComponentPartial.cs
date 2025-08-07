9a6d52e17592ad33840b7229feb68b03430ec942
mahirbugray <74426348+mahirbugray@users.noreply.github.com>
2025-08-07 03:22:31 +0300

    UI-Layouts-Edit

---
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_CategoriesDefaultComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _CategoriesDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
